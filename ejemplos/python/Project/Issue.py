d8bc58b683ae59e2027c0a51fcae37a527020084
JJ Merelo <jjmerelo@gmail.com>
2020-03-05 09:46:29 +0100

    Adds project name

--- ejemplos/python/Project/Issue.py
+++ ejemplos/python/Project/Issue.py
@@ -4,8 +4,9 @@ IssueState = Enum('IssueState', 'Open Closed')
 
 class Issue:
 
-    def __init__(self):
+    def __init__(self, projectName):
         self.state = IssueState.Open
+        self.projectName = projectName
 
     def close(self):
         self.state = IssueState.Closed
--- ejemplos/python/Project/tests/test_issue.py
+++ ejemplos/python/Project/tests/test_issue.py
@@ -1,11 +1,16 @@
 import pytest
 from Project.Issue import Issue, IssueState
 
+PROJECTNAME = "testProject"
+
 @pytest.fixture
 def issue():
-    issue = Issue()
+    issue = Issue(PROJECTNAME)
     return issue
 
+def test_has_name_when_created(issue):
+    assert issue.projectName  == PROJECTNAME
+    
 def test_is_open_when_created(issue):
     assert  issue.state == IssueState.Open
 
