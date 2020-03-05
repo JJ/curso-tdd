aa2346ced927f17f6b009687811a683c76e4cab3
JJ Merelo <jjmerelo@gmail.com>
2020-03-05 09:53:36 +0100

    Convierte todos los atributos en privados
    Y ahora fallan todos los tests, como debe ser :-)
    La encapsulación es importante.

--- ejemplos/python/Project/Issue.py
+++ ejemplos/python/Project/Issue.py
@@ -5,12 +5,12 @@ IssueState = Enum('IssueState', 'Open Closed')
 class Issue:
 
     def __init__(self, projectName: str, issueId: int ):
-        self.state = IssueState.Open
-        self.projectName = projectName
-        self.issueId = issueId
+        self._state = IssueState.Open
+        self._projectName = projectName
+        self._issueId = issueId
 
     def close(self):
-        self.state = IssueState.Closed
+        self._state = IssueState.Closed
 
     def reopen(self):
-        self.state = IssueState.Open
+        self._state = IssueState.Open
