33b845d9b0381de9b62654ce769dde23a0b0f069
JJ Merelo <jjmerelo@gmail.com>
2020-09-10 17:31:51 +0200

    Passing tests

--- ejemplos/rust/issue/src/main.rs
+++ ejemplos/rust/issue/src/main.rs
@@ -1,17 +1,17 @@
 #[derive(Debug)]
-enum IssueState {
+pub enum IssueState {
     Open,
     Closed,
 }
 
 #[derive(Debug)]
-struct Issue {
+pub struct Issue {
     state: IssueState,
     project_name: String,
     issue_id: u64,
 }
 
-fn issue_factory( project_name: String,
+pub fn issue_factory( project_name: String,
                   issue_id: u64) -> Issue {
     return Issue { state: IssueState::Closed,
                    project_name: project_name,
@@ -24,4 +24,13 @@ fn main() {
     let mut that_issue = issue_factory( String::from("CoolProject"), this_issue.issue_id + 1 );
     that_issue.state = IssueState::Open; // Avoid warning
     println!("{:?}", that_issue);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_creation() {
+        assert_eq!(issue_factory(String::from("CoolProject"), 1 ).issue_id, 1);
+    }
 }
