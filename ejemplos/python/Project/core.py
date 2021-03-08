b6532111c1056d652e8a677566ec0b2b45e89495
JJ Merelo <jjmerelo@gmail.com>
2021-03-08 13:10:57 +0100

    Quitanto olores :sparkles:

--- ejemplos/python/Project/core.py
+++ ejemplos/python/Project/core.py
@@ -1,21 +1,32 @@
+"""
+Project tendrá todo lo relacionado con los proyectos de estudiantes en una asignatura.
+"""
+
+
 class Project:
+    """
+    Clase contenedor de todo lo relacionado con un proyecto en GitHub
+    """
+    def __init__(self, name: str ):
+        self._name = name
 
-    def __init__(self, name ):
-        self.name = name
-
+    @property
     def name(self):
-        return self.name
+        """
+        Simplemente el nombre del proyecto
+        """
+        return self._name
 
-    def newMilestone(self, milestone):
-        pass;
+    def new_milestone(self, milestone):
+        pass
 
     def milestones(self):
         pass
 
-    def percentageCompleted(self):
+    def percentage_completed(self):
         pass
 
-    def completionSummary(self):
+    def completion_summary(self):
         pass
 
     def data(self):
